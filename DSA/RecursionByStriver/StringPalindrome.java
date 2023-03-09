ca3a8f35c27148acffbe39953c0593ca8d1af13d
Mahesh Navghane <mnavghane66@gmail.com>
2023-03-10 04:01:24 +0530

    "StringPalindrome is program is added"

--- DSA/RecursionByStriver/StringPalindrome.java
+++ DSA/RecursionByStriver/StringPalindrome.java
@@ -1,5 +1,38 @@
 package DSA.RecursionByStriver;
 
 public class StringPalindrome {
-  
+  public static void main(String[] args) {
+    
+    System.out.println(isPalindrome("wow"));
+    System.out.println("isPalindromeRec: "+isPalindromeRec("wow",0));
+    System.out.println(isPalindromeRec("1112111", 0));
+    System.out.println(isPalindromeRec("Limit", 0));
+  }
+
+  public static boolean isPalindrome(String str){
+
+    int l = str.length()-1;
+
+    String newStr = "";
+    for(int i =l; i>=0; i--){
+      
+      newStr += str.charAt(i);
+    }
+
+    if(newStr.equals(str))
+    return true;
+
+    return false;
+  }
+
+  public static boolean isPalindromeRec(String str, int i){
+
+    if(i>str.length()/2)
+    return true;
+
+    if(str.charAt(i) != str.charAt(str.length() - 1 -i))
+    return false;
+
+    return isPalindromeRec(str, i+1);  
+  }
 }
