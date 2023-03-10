9fbac2803d12a2933b87353ed93f547b45291e3c
Mahesh Navghane <mnavghane66.gmail.com>
2023-03-10 14:27:38 +0530

    "Fibonacci is added for recursion and looping"

---
+++ DSA/RecursionByStriver/Fibonacci.java
@@ -0,0 +1,35 @@
+package DSA.RecursionByStriver;
+
+public class Fibonacci {
+    
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(fib(n));
+        System.out.println(fibRec(n));
+    }
+
+   
+
+    public static int fib(int n){
+
+        int num1 = 0;
+        int num2 = 1;
+
+        int fib = 0;
+        for(int i=1; i<n;i++){
+            fib = num1+num2;
+            num1 = num2;
+            num2 = fib;
+        }
+
+        return fib;
+    }
+
+    public static int fibRec(int n){
+
+        if(n==1 || n==0)
+        return n;
+
+        return fibRec(n-1)+fibRec(n-2);
+    }
+}
